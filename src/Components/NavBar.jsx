fe3edc104bbcf4f9a3828ca3447986b1fc921219
Aditya Bhattacharjee <adityab76@gmail.com>
2023-12-10 13:21:26 +0530

    landing page updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,52 +1,62 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { VscAccount } from "react-icons/vsc";
+
 function NavBar() {
-  
-  const [authPopup,setAuthPopUp] = useState(false);
+  const [authPopup, setAuthPopUp] = useState(false);
 
-
-  const handlepopup =()=>{
-    setAuthPopUp(true) ; 
-    
-  }
-  const handlepopupClose =()=>{
-    setAuthPopUp(false)
+  const handlePopup = () => {
+    setAuthPopUp(true);
   }
 
+ 
 
-  const renderPopup=()=>{
-    if(authPopup === true){
-      return(
-    
-        <div className="w-full h-full bg-zinc-100 absolute z-3">
-              <span className='bg-zinc-50 p-10 border-solid'>
-                 <ul className='NavBar'><li>LOGIN</li></ul>
-              </span>
+  useEffect(() => {
+    let timer;
+
+timer = setTimeout(()=>[
+  setAuthPopUp(false)
+],10000)
+
+  }, [authPopup]);
+
+  const renderPopup = () => {
+    if (authPopup === true) {
+      return (
+        <div className="w-full h-full absolute z-3">
+          <ul className='text-white mt-3 ml-10'>
+            <li className='item'>
+              <Link to='/Login' className='cursor-pointer '>
+                LOGIN
+              </Link>
+            </li>
+            <li className='mt-3 item'>
+              <Link to='/Register' className='cursor-pointer '>
+                REGISTER
+              </Link>
+            </li>
+          </ul>
         </div>
-        
-      )
+      );
     }
   }
 
   return (
     <>
-    <nav className='NavBar'>
-        
-
-        <ul className='navigation-list'>
-<Link to='/Store' style={{textDecoration:"none"}}><li className='item'>STORE</li></Link>
-<li className='item' onMouseEnter={handlepopup}
- onMouseLeave={handlepopupClose}>< VscAccount style={{width:"30px",height:"30px"}}/></li>
-
-
+      <nav className='NavBar'>
+        <ul className='navigation-list ml-5'>
+          <Link to='/Store' style={{ textDecoration: "none" }}>
+            <li className='item py-5'>STORE</li>
+          </Link>
+          <li className='item' onClick={handlePopup}>
+            <VscAccount style={{ width: "30px", height: "30px" }} />
+          </li>
         </ul>
         {renderPopup()}
-    </nav>
-    
+      </nav>
     </>
   )
 }
 
-export default NavBar
+export default NavBar;
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import {Routes,Route} from 'react-router-dom' ; 
 import HomePage from '../Pages/HomePage';
 import Store from '../Pages/Store';
+import Login from '../Pages/Login';
+import Register from '../Pages/Register';
+
 function AllRoutes() {
     return (
       <>
       <Routes>
       <Route path='/' element={<HomePage/>}></Route>
       <Route path='/Store' element={<Store/>}></Route>
+      <Route path='/Login' element={<Login/>}></Route>
+      <Route path='/Register' element={<Register/>}></Route>
   
       </Routes>
       
