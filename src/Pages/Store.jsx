2407604dc8ed53d1eaaef98b0e364738d66e14d1
Aditya Bhattacharjee <adityab76@gmail.com>
2023-12-12 13:41:03 +0530

    Store Page added , Footer Added

--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -2,7 +2,39 @@ import React from 'react';
 import StoreBackground from '../Media/store.png';
 import Logo from "../Media/Logo.png";
 import {Link} from 'react-router-dom' ; 
+import Footer from '../Components/Footer';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 function Store() {
+
+
+const StoreCardData = [
+  {
+    id:"1",
+    CardName:"GYM SUPPLEMENT STORE",
+    CardDesc:"",
+    
+  },
+  {
+    id:"2",
+    CardName:"GENERAL HEALTH SUPPLEMENT STORE",
+    CardDesc:"",
+    
+  },
+  {
+    id:"3",
+    CardName:"AYURVEDIC SUPPLEMENT STORE",
+    CardDesc:"",
+    
+  },
+ 
+]
+
+
   return (
     <>
       <div className="relative">
@@ -38,6 +70,40 @@ function Store() {
       <div>
        
       </div>
+      <section 
+      
+      >
+
+        <div className="bg-red-200 shadow-md h-96 flex items-center justify-around">
+
+          {StoreCardData.map((item)=>{
+
+          return(
+        <Card sx={{ maxWidth: 345,maxHeight:350 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {item.CardName}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">VISIT STORE </Button>
+        
+      </CardActions>
+    </Card>
+          )
+    })}
+        </div>
+      </section>
+      <Footer/>
     </>
   );
 }
